1894c0670f9e9552fda9147608955554cde1ed48
Jakub Wegrzynek <kuba.lysy.weg@gmail.com>
2023-11-09 10:03:23 +0100

    next lesson

--- Object-Oriented/OOP1.py
+++ Object-Oriented/OOP1.py
@@ -1,11 +1,15 @@
 import random
 import matplotlib.pyplot as plt
 
+SIGMA = 0.02
+
 
 class Creature:
-    p_death = 0.2
-    p_reproduce = 0.2
     alive = True
+
+    def __init__(self, p_death=0.2, p_reproduce=0.2):
+        self.p_death = p_death
+        self.p_reproduce = p_reproduce
 
     def natural_selection(self):
         if random.random() <= self.p_death:
@@ -13,7 +17,19 @@ class Creature:
 
     def reproduce(self):
         if random.random() <= self.p_reproduce:
-            return Creature()
+            return Creature(self.p_death + random.normalvariate(sigma=SIGMA),
+                            self.p_reproduce + random.normalvariate(sigma=SIGMA))
+
+    @property
+    def p_death(self):
+        return self.p_death
+
+    @p_death.setter
+    def p_death(self, value):
+        if value < 0:
+            self._p_death = 0
+        else:
+            self._p_death = value
 
 
 class Population:
@@ -44,11 +60,17 @@ class Population:
         plt.title("Plot of population")
         plt.show()
 
+    def plot_attribute(self, attribute='p_death'):
+        values = [getattr(creature, attribute) for creature in self.speciemen]
+        plt.hist(values)
+        plt.show()
+
 
 p1 = Population()
 
 print(p1.count_alive())
-p1.simulate(150)
+p1.simulate(75)
 print(p1.count_alive())
 print(p1.history)
-p1.plot()
+# p1.plot()
+p1.plot_attribute('p_reproduce')
